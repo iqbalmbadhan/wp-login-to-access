038e0afa1b404d42cd653c4d88159d314590e8d2
Iqbal Mahmud <70919494+iqbalmbadhan@users.noreply.github.com>
2023-10-08 00:26:04 +0600

    Update logintoaccess.php

--- logintoaccess.php
+++ logintoaccess.php
@@ -99,9 +99,4 @@ function my_login_logo_url() {
 }
 add_filter( 'login_headerurl', 'my_login_logo_url' );
 
-function my_login_logo_url_title() {
-    return 'Your Site Name and Info';
-}
-add_filter( 'login_headertext', 'my_login_logo_url_title' );
-
 ?>
